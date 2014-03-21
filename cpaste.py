101888073451eeba237f3682f76c15873bb1e58c
Cydrobolt <summermontreal@gmail.com>
2014-03-21 18:12:41 -0400

    Templating, POST&GET, commit

--- cpaste.py
+++ cpaste.py
@@ -1,8 +1,7 @@
 
 #CPaste - v.0.1
 
-from flask import Flask
-from flask import render_template
+from flask import Flask,render_template,request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 #Init SQLAlchemy
@@ -15,6 +14,14 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return render_template("index.html")
+
+@app.route("/paste", methods=['GET','POST'])
+def paste():
+	pname = request.form['pname']
+	pcontent = request.form['pcontent']
+	return render_template('paste.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
